f7bb42f707f855437c6eb24cf566c364cf5ebfff
ophel1ac <Vic.fominov@gmail.com>
2020-09-19 22:35:19 +0300

    Renamed directory

--- Page Object Model/test_main_page.py
+++
@@ -1,9 +0,0 @@
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-    
-def

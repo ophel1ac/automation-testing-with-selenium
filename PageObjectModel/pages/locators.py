6da9bc47b3f265e6e00d830414c34a8223ff2a2b
ophel1ac <Vic.fominov@gmail.com>
2020-09-21 14:38:14 +0300

    test methods and test case for product page

--- PageObjectModel/pages/locators.py
+++ PageObjectModel/pages/locators.py
@@ -7,5 +7,12 @@ class MainPageLocators:
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-
     REG_FROM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form [type='submit']")
+    CONFIRM_ADDING = (By.CSS_SELECTOR, ".alertinner strong")
+    BOOK_TITLE = (By.CSS_SELECTOR, ".product_main h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
---
+++ PageObjectModel/pages/product_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        basket_button.click()
+
+    def should_be_right_book_added(self):
+        book_title = self.browser.find_element(*ProductPageLocators.BOOK_TITLE)
+        confirm_message = self.browser.find_element(*ProductPageLocators.CONFIRM_ADDING)
+        assert book_title.text == confirm_message.text, \
+            "Wrong book added, wrong title." \
+            f"\nExpected {book_title.text}, result {confirm_message.text}"
+
+    def should_be_right_book_price(self):
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL)
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        assert basket_total.text == book_price.text, \
+            "Wrong book added, wrong price." \
+            f"\nExpected {book_price.text}, result {basket_total.text}"
